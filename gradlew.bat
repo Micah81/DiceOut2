b69c0ca27e98a70cfee1cd569b750f5dd3e37261
Micah81 <micahmrocks@gmail.com>
2018-11-26 20:37:45 -0700

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,52 @@
+<application>
+  <component name="RecentProjectsManager">
+    <option name="recentPaths">
+      <list>
+        <option value="$USER_HOME$/Desktop/Ex_Files_App_AndroidSDK/DiceOut" />
+        <option value="$USER_HOME$/Desktop/Ex_Files_App_AndroidSDK/BalloonPopper" />
+        <option value="$USER_HOME$/Desktop/Ex_Files_App_AndroidSDK/fs" />
+      </list>
+    </option>
+    <option name="openPaths">
+      <list>
+        <option value="$USER_HOME$/Desktop/Ex_Files_App_AndroidSDK/DiceOut" />
+      </list>
+    </option>
+    <option name="lastPath" value="$USER_HOME$/Desktop/Ex_Files_App_AndroidSDK/DiceOut" />
+    <option name="pid" value="" />
+    <option name="additionalInfo">
+      <map>
+        <entry key="C:/AndroidAppDev/AnonCharts/android">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-173.4720617" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1527390383549" />
+              <option name="buildTimestamp" value="1523654851859" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="C:/AndroidAppDev/ChartSentiment">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-173.4720617" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1537081542851" />
+              <option name="buildTimestamp" value="1523654871137" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="C:/AndroidAppDev/ChartVoter">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-173.4720617" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1527393433580" />
+              <option name="buildTimestamp" value="1523654868942" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+      
